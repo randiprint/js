34951d760047234f70380b35c300c001bd466fa7
randiprint <randiafif0035@mhs.unisbank.ac.id>
2025-10-31 06:42:47 +0700

    first commit

---
+++ 2/belajar2.js
@@ -0,0 +1,3 @@
+document.getElementById("nama").innerHTML="Halo, nama saya Randi";
+document.getElementById("hobi").innerHTML="Halo, hobi saya main komputer";
+document.getElementById("cita-cita").innerHTML="Halo, cita-cita saya programming dan IT Networking";
